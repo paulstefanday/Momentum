94d439540e8f00622cd27bb56c962bc485fb1735
Paul Day <paul@paulday.com.au>
2014-12-02 23:57:11 +1100

    replaced sass with less and added styles into app folder

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,27 +1,37 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jade = require('gulp-jade');
-var sass = require('gulp-sass');
 var htmlify = require('gulp-angular-htmlify');
 var templateCache = require('gulp-angular-templatecache');
-// var mainBowerFiles = require('main-bower-files');
-// gulp.task('bower', function() {
-//     return gulp.src(mainBowerFiles())
-//         .pipe(gulp.dest('js/bower.js'))
-// });
+var path = require('path');
+var less = require('gulp-less');
 
 var paths = {
   scripts: ['common/**/*.js', 'app/**/*.js'],
   templates: 'app/**/*.jade',
   // images: 'client/img/**/*',
-  css: 'styles/**/*.scss',
+  css: [ 'styles/**/*.less', 'app/**/*.less'],
   root: [ '../assets/js/' , '../assets/styles/']
 };
 
-gulp.task('sass', function () {
-    gulp.src(paths.css)
-        .pipe(sass())
-        .pipe(gulp.dest(paths.root[1]));
+gulp.task('less', function () {
+    // gulp.src('styles/style.scss')
+    //     .pipe(sass())
+    //     .pipe(concat('style.css'))
+    //     .pipe(gulp.dest(paths.root[1]));
+
+
+  gulp.src(paths.css)
+    .pipe(concat('style.less'))
+    .pipe(less({
+      // paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest(paths.root[1]));
+
+
+
+
 });
 
 gulp.task('js-common', function() {
@@ -52,8 +62,8 @@ gulp.src(paths.templates)
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['js-common', 'js-app']);
   gulp.watch(paths.templates, ['templates']);
-  gulp.watch(paths.css, ['sass']);
+  gulp.watch(paths.css, ['less']);
 });
 
 
-gulp.task('default', ['js-app', 'templates', 'js-common', 'sass', 'watch']);
+gulp.task('default', ['js-app', 'templates', 'js-common', 'less', 'watch']);
